8ecffec5e2847ffd7e9d3a15a311005e63c3673b
syhrl <syhrlanwr@gmail.com>
2025-04-17 22:47:15 +0700

    add home-manager to flake

--- flake.nix
+++ flake.nix
@@ -4,9 +4,14 @@
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs @ { self, nixpkgs, home-manager, ... }:
+    {
     # Please replace my-nixos with your hostname
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -14,6 +19,14 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.syhrl = import ./home.nix;
+          };
+        }
       ];
     };
   };
